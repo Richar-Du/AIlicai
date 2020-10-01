f1b2f72496135d087be7ed2bdd5529bd669f5b0f
sheng <2578064523@qq.com>
2020-10-01 17:17:14 +0800

    market

--- miniprogram/pages/market/market.js
+++ miniprogram/pages/market/market.js
@@ -5,14 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    high:['004997','005968','005969','004640','000746','001576','005620','001790','003745','501009'],
+    medium:['000977','000336','002190','002542','001606','168501','001218','005689','162703','005454'],
+    low:['000297','001045','163816','360013','000014','519967','320021','210014','005246','003401'],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let _this = this
+    const items = []
+    const code = _this.data.high
+    for (var i=0;i<code.length;i++) {
+      wx.request({
+        url: 'https://www.ywonchall.top:90/', //仅为示例，并非真实的接口地址
+        data: {
+          code:code[i]
+        }, 
+        method:'POST',
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success (res) {
+          const data = res.data
+          data['gszzl'] = parseFloat(data['gszzl'])
+          console.log(data)
+          items.push(data)
+          _this.setData({
+            fund:items
+          })
+        }
+      })
+    }
+    _this.setData({
+      fund:items
+    })
   },
 
   /**
